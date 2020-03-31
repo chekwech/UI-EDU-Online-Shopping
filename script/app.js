05096c9d05dff55cf8a8896172fa42d7c1334e0d
mirriam-akongo <jemmirriako@gmail.com>
2020-03-31 23:54:57 +0300

    Added an upload page

--- script/app.js
+++ script/app.js
@@ -37,3 +37,8 @@ function validate(){
         return true;
     }
 }
+
+var loadFile = function(event){
+    var image = document.getElementById("output");
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
